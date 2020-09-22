c38f048159d5ef6327e517d754067e3f16e2a6eb
Christopher Brine <chrisbrine1999@gmail.com>
2020-09-22 18:31:53 -0400

    Added BTPlayer and it's Test Child Class

---
+++ Engine/Engine/Battle/TestBattlePlayer.cpp
@@ -0,0 +1,25 @@
+#include "TestBattlePlayer.h"
+
+#include <iostream>
+
+RTTR_REGISTRATION{
+	registration::class_<TestBattlePlayer>("TestBattlePlayer")
+		.public_object_constructor;
+}
+
+TestBattlePlayer::TestBattlePlayer() { }
+
+TestBattlePlayer::~TestBattlePlayer() { }
+
+void TestBattlePlayer::Start() {
+	SetMaxHitpoints(20);
+	SetHitpoints(20);
+}
+
+void TestBattlePlayer::Update() {
+
+}
+
+void TestBattlePlayer::LateUpdate() {
+
+}
