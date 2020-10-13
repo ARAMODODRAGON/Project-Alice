4eeec5707de6a9596ce28da393f346a2c220df0f
Domara Shlimon <domara2000@gmail.com>
2020-10-13 13:22:11 -0400

    Started network manager

--- Engine/Engine/General/Types.hpp
+++ Engine/Engine/General/Types.hpp
@@ -1,7 +1,18 @@
 #ifndef _GENERAL_TYPES_HPP
 #define _GENERAL_TYPES_HPP
 
-typedef unsigned char ubyte;
+// we want a bunch of integer types
+#include <inttypes.h>
+typedef uint64_t uint64;
+typedef uint32_t uint32;
+typedef uint16_t uint16;
+typedef int64_t  int64;
+typedef uint8_t  uint8;
+typedef int32_t  int32;
+typedef int16_t  int16;
+typedef int8_t   int8;
+
+typedef uint8_t ubyte;
 
 #include <string>
 #include <array>
---
+++ Engine/Engine/Network/NetworkManager.hpp
@@ -0,0 +1,15 @@
+#ifndef NETWORK_NETWORK_MANAGER_HPP
+#define NETWORK_NETWORK_MANAGER_HPP
+#include "../General/Macros.hpp"
+#include "../General/Types.hpp"
+
+// is this a client / server 
+enum class NSystemType : uint8 {
+	None,
+	Client,
+	Server
+};
+
+
+
+#endif // !NETWORK_NETWORK_MANAGER_HPP
