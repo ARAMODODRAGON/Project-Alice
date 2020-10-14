006d2cc2f7d21bdadf8c9d662605918a82611abe
Christopher Brine <chrisbrine1999@gmail.com>
2020-10-14 17:48:10 -0400

    Added framework for attack/defence spells

---
+++ Engine/Engine/Battle/Spells/Attack/BTAttackSpell.hpp
@@ -0,0 +1,24 @@
+#ifndef BATTLE_ATTACK_SPELL_HPP
+#define BATTLE_ATTACK_SPELL_HPP
+
+#include "../BTSpell.hpp"
+#include "../../ShootComponent.hpp"
+
+class BTAttackSpell : public BTSpell {
+	rttr::type attackType;
+	int allocateCount;
+
+	ShootComponent* shootComponent;
+public:
+	BTAttackSpell(rttr::type _type, int _count);
+	~BTAttackSpell() override;
+
+	void Update();
+
+	ShootComponent* GetShootComponent();
+	void SetShootComponent(ShootComponent* _component;
+
+	RTTR_ENABLE(BTSpell) RTTR_REGISTRATION_FRIEND
+};
+
+#endif
