ecb08ddb452ce21590465fbd2d885a53ece72eb2
Domara Shlimon <domara2000@gmail.com>
2020-11-24 20:39:30 -0500

    a couple more changes

--- Project-Alice/Scripts/Levels/BattleDemo/BattleDemoCamera.cpp
+++ Project-Alice/Scripts/Levels/BattleDemo/BattleDemoCamera.cpp
@@ -1,4 +1,6 @@
 #include "BattleDemoCamera.hpp"
+#include <Engine\Battle\BattleManager.hpp>
+#include <Engine\Input\Keyboard.hpp>
 
 RTTR_REGISTRATION {
 	registration::class_<BattleDemoCamera>("BattleDemoCamera")
@@ -18,11 +20,45 @@ void BattleDemoCamera::Start() {
 	// created ui renderer
 	if (ui = AddComponent<UIRenderer>()) {
 		// init ui here
+		ui->AddSprite("default");
 	}
 }
 
 void BattleDemoCamera::Update() { }
 
-void BattleDemoCamera::LateUpdate() { }
+void BattleDemoCamera::LateUpdate() {
+	//Rect levelBounds = BattleManager::GetBattleArea();
+	//constexpr float width = 10.0f;
+
+	// draw the level boundries
+	//Rect left;
+	//left.min.y = levelBounds.min.y;
+	//left.max.y = levelBounds.max.y;
+	//left.max.x = levelBounds.min.x;
+	//left.min.x = left.max.x - width;
+	//ui->DrawSprite("default", left.min.x, left.min.y, left.max.x, left.max.y);
+	//
+	//Rect right;
+	//right.min.y = levelBounds.min.y;
+	//right.max.y = levelBounds.max.y;
+	//right.max.x = levelBounds.max.x + width;
+	//right.min.x = right.max.x;
+	//ui->DrawSprite("default", right.min.x, right.min.y, right.max.x, right.max.y);
+	//
+	//Rect top;
+	//top.min.x = levelBounds.min.x;
+	//top.max.x = levelBounds.max.x;
+	//top.min.y = levelBounds.max.y;
+	//top.max.y = top.min.y + 10;
+	//ui->DrawSprite("default", top.min.x, top.min.y, top.max.x, top.max.y);
+	//
+	//Rect bottom;
+	//bottom.min.x = levelBounds.min.x;
+	//bottom.max.x = levelBounds.max.x;
+	//bottom.max.y = levelBounds.min.y;
+	//bottom.min.y = bottom.max.y - 10;
+	//ui->DrawSprite("default", bottom.min.x, bottom.min.y, bottom.max.x, bottom.max.y);
+
+}
 
 void BattleDemoCamera::OnDestroy() { }
--- Project-Alice/Scripts/Levels/BattleDemo/BattleDemoLevel.cpp
+++ Project-Alice/Scripts/Levels/BattleDemo/BattleDemoLevel.cpp
@@ -1,4 +1,5 @@
 #include "BattleDemoLevel.hpp"
+#include <Engine\Battle\BattleManager.hpp>
 
 RTTR_REGISTRATION {
 	registration::class_<BattleDemoLevel>("BattleDemoLevel")
@@ -11,7 +12,15 @@ BattleDemoLevel::~BattleDemoLevel() { }
 
 void BattleDemoLevel::Init() {
 	
+	// setup camera
 	camera = ObjectFactory::Make<BattleDemoCamera>();
+
+	// setup the bounds of the level
+	Rect levelBounds;
+	constexpr float halfHeight = (288.0f / 2.0f) - 20.0f;
+	levelBounds.max = vec2(halfHeight * 0.88, halfHeight);
+	levelBounds.min = -levelBounds.max;
+	BattleManager::SetBattleArea(levelBounds);
 
 }
 
