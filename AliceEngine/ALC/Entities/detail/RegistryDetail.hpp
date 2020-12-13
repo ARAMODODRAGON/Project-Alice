13a6fab7abb9b85d8b6837e404f7ed1e713c1aa9
Domara Shlimon <domara2000@gmail.com>
2020-12-13 02:47:50 -0500

    moved IComponent into namespace ALC::detail and changed the name to component

--- AliceEngine/ALC/Entities/detail/RegistryDetail.hpp
+++ AliceEngine/ALC/Entities/detail/RegistryDetail.hpp
@@ -2,36 +2,36 @@
 #define ALC_ENTITIES_DETAIL_COMPONENTTYPEINFO_HPP
 
 // declares that the struct or class is a component
-#define ALC_Component public ::ALC::IComponent
+#define ALC_Component public ::ALC::detail::component
 
 namespace ALC {
 
 	class Registry;
 	class Behavior;
 
-	struct IComponent {
-		virtual ~IComponent() = 0 { }
-	};
-
 	namespace detail {
+
+		struct component {
+			virtual ~component() = 0 { }
+		};
 
 		template<typename T>
 		using is_behavior_t = std::enable_if_t<std::is_base_of_v<Behavior, T>>;
 
 		template<typename T>
-		using is_component_t = std::enable_if_t<std::is_base_of_v<IComponent, T>>;
+		using is_component_t = std::enable_if_t<std::is_base_of_v<component, T>>;
 
 		template<typename... T>
 		struct are_components;
 
 		template<typename T0, typename... T>
 		struct are_components<T0, T...> {
-			static constexpr bool value = std::is_base_of_v<IComponent, T0> && are_components<T...>;
+			static constexpr bool value = std::is_base_of_v<component, T0> && are_components<T...>;
 		};
 
 		template<typename T>
 		struct are_components<T> {
-			static constexpr bool value = std::is_base_of_v<IComponent, T>;
+			static constexpr bool value = std::is_base_of_v<component, T>;
 		};
 
 		template<typename... T>
