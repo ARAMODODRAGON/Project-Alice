69e79a76a6f4300e1c6e74fc6eb6ce02ecddc16c
ad83436 <45110755+ad83436@users.noreply.github.com>
2021-03-26 13:37:13 -0400

    minor change to start pos of enemy

--- AliceGame/Game/Enemies/RuiEnemy.cpp
+++ AliceGame/Game/Enemies/RuiEnemy.cpp
@@ -125,7 +125,7 @@ void RuiEnemy::BattleBegin() {
 void RuiEnemy::PreBattleBegin(const Phase lastphase, ALC::Entity self, ALC::Timestep ts) {
 
 	auto [transform, cbody] = self.GetComponent<ALC::Transform2D, CharacterBody>();
-	transform.position = ALC::vec2(BattleManager::GetLevelBounds().top + 45, 0);
+	transform.position = ALC::vec2(0, BattleManager::GetLevelBounds().top + 45);
 	moveStates.push_back(MoveStates::States::Move);
 }
 void RuiEnemy::PreBattleStep(ALC::Entity self, ALC::Timestep ts) {
