eb469aa392b001efb7dc1527025c85a706054785
ad83436 <45110755+ad83436@users.noreply.github.com>
2021-03-26 13:36:04 -0400

    || to && in ruienemy late update

--- AliceGame/Game/Systems/MoveStates.hpp
+++ AliceGame/Game/Systems/MoveStates.hpp
@@ -25,6 +25,10 @@ public:
 	bool GetIsComplete() { return isComplete; }
 	void SetIsComplete(bool isComplete_) { isComplete = isComplete_; }
 
+
+	//Function that moves the AI to a desired destination either all the way to the bounds r over a certian amount of time 
+	//takes the current state and assigns it a next state if one is given and returns it 
+	//uses BTA moveTo function to move the AI
 	void PerformMoveState(Enemy* enemy, States moveState, ALC::uint8 *_curntState, ALC::Timestep ts, 
 		                  float moveTime = NULL, ALC::uint8 nextState = 0,ALC::vec2 dest = ALC::vec2(0,0));
 
